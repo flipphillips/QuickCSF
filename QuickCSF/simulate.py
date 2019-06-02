37545f70ff9a66df2e15dc398d32c654d326e756
flip phillips <flip@skidmore.edu>
2019-06-02 09:06:22 -0400

    argv to widget

--- QuickCSF/simulate.py
+++ QuickCSF/simulate.py
@@ -227,7 +227,7 @@ if __name__ == '__main__':
 	if settings['trials'] is None:
 		from qtpy import QtWidgets
 		from . import ui
-		app = QtWidgets.QApplication()
+		app = QtWidgets.QApplication(sys.argv)
 		settings = ui.getSettings(parser, settings, ['trials'])
 
 	if settings is not None:
